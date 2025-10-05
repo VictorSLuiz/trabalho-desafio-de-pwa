1c813d24bc61d26c1438cf08645967a3ac84a6ec
Victor Silva <victorsilvaluiz10@gmail.com>
2025-10-05 19:19:49 -0300

    feat: add measurement converter web app with PWA support
    - Created index.html for the measurement converter interface, including input fields for user interaction and a button to calculate conversions between meters and centimeters.
    - Implemented basic styling for a modern and user-friendly design.
    - Added manifest.json to define the web app's metadata and icons for PWA capabilities.
    - Developed service-worker.js to enable caching of essential files for offline access and improved performance.

---
+++ src/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("Conversor-cache").then(cache => {
+            return cache.addAll([
+                "index.html",
+                "manifest.json",
+                "192x192.png",
+                "512x512.png"
+            ])
+        })
+    )
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
